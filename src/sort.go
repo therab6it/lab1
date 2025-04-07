978125d00ae51963f277a062284961034f0a1be8
raopranav <97642829+therab6it@users.noreply.github.com>
2025-04-06 20:04:35 -0700

    Test
    woops

--- src/sort.go
+++ src/sort.go
@@ -8,9 +8,18 @@ import (
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	test()
+
 	if len(os.Args) != 3 {
 		log.Fatalf("Usage: %v inputfile outputfile\n", os.Args[0])
 	}
 
 	log.Printf("Sorting %s to %s\n", os.Args[1], os.Args[2])
+}
+
+func test() {
+
+	// This is a test function to ensure the package compiles.
+	// You can add test cases here if needed.
+	log.Println("This is a test function in the sort package.")
 }
