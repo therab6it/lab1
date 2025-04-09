f1e194235120fa6a74f38e8e97edda9f7aac8054
raopranav <97642829+therab6it@users.noreply.github.com>
2025-04-09 15:12:32 -0700

    Wait I think I fixed it (I peed)
    - Updated record from [][]byte to struct
    - Using io.ReadFull instead of Read from io.Reader to ensure buffers are always full
    - Writing bytes of record by parts instead of full record at once

--- src/sort.go
+++ src/sort.go
@@ -25,6 +25,12 @@ func WriteBigEndianUint32(buffer []byte, num uint32) {
 	binary.BigEndian.PutUint32(buffer, num)
 }
 
+type Record struct {
+	length []byte
+	key    []byte
+	value  []byte
+}
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
@@ -36,65 +42,75 @@ func main() {
 
 	inputFile := os.Args[1]
 	outputFile := os.Args[2]
+	var err error
 
 	inFile, err := os.Open(inputFile)
 	if err != nil {
-		log.Fatalf("Error opening input file: %v\n", err)
+		panic(err)
 	}
 	defer inFile.Close()
 
 	len_buffer := make([]byte, 4)
 	key_buffer := make([]byte, 10)
-	var records [][]byte
+	var records []Record
 
 	/* Store records in memory */
 	for {
 		// Read the length of the record
-		l, err := inFile.Read(len_buffer)
+		_, err := io.ReadFull(inFile, len_buffer)
 		// Read until EOF
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			log.Fatalf("Error reading length: %v\n", err)
+			log.Fatalf("Error reading input file: %v\n", err)
 		}
-		length := ReadBigEndianUint32(len_buffer[:l])
+		length := ReadBigEndianUint32(len_buffer[:])
 		// Read the key
-		k, err := inFile.Read(key_buffer)
-		if err != nil || k != 10 {
+		_, err = io.ReadFull(inFile, key_buffer)
+		if err != nil {
 			log.Fatalf("Error reading key: %v\n", err)
 		}
 		// Read the value
-		val_buffer := make([]byte, length-14)
-		v, err := inFile.Read(val_buffer)
+		val_buffer := make([]byte, length-10)
+		_, err = io.ReadFull(inFile, val_buffer)
 		if err != nil {
 			log.Fatalf("Error reading value: %v\n", err)
 		}
-		value := val_buffer[:v]
 
 		// Store the record
-		var record []byte
-		record = append(record, len_buffer[:]...)
-		record = append(record, key_buffer[:]...)
-		record = append(record, value...)
+		record := Record{
+			length: append([]byte{}, len_buffer...),
+			key:    append([]byte{}, key_buffer...),
+			value:  append([]byte{}, val_buffer...),
+		}
 		records = append(records, record)
 	}
 
-	/* Sort the records in memory */
+	/* Sort the records */
 	sort.Slice(records, func(i, j int) bool {
-		return bytes.Compare(records[i][4:14], records[j][4:14]) < 0
+		return bytes.Compare(records[i].key, records[j].key) < 0
 	})
 
 	/* Write sorted records to output file */
 	outFile, err := os.Create(outputFile)
 	if err != nil {
-		log.Fatalf("Error opening input file: %v\n", err)
+		panic(err)
 	}
+	defer outFile.Close()
+
 	for _, record := range records {
-		n, err := outFile.Write(record)
-		if err != nil || n != len(record) {
+		_, err = outFile.Write(record.length)
+		if err != nil {
+			log.Fatalf("Error writing length: %v\n", err)
+		}
+		_, err = outFile.Write(record.key)
+		if err != nil {
+			log.Fatalf("Error writing key: %v\n", err)
+		}
+		_, err = outFile.Write(record.value)
+		if err != nil {
 			log.Fatalf("Error writing record: %v\n", err)
 		}
 	}
-	defer outFile.Close()
 }
